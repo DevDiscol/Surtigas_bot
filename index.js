deb4777c7b5084b08b4d3aba82d1f7c14ba83800
Andres Morales <morales.a.h.98@gmail.com>
2024-03-12 07:59:09 -0500

    dockerizacion  del proyecto

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const { Telegraf } = require("telegraf");
 const { message } = require("telegraf/filters");
 const XLSX = require("xlsx");
+const http = require("http");
+
+const hostname = "0.0.0.0";
+
+const port = 3000; 
+
+const server = http.createServer((req, res)=>{
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end('Bienvenid@s al canal de aprendevops');
+});
+
+server.listen(port, hostname, ()=>{
+   console.log(`El servidor se esta ejecutando en el https://${hostname}:${port}/`);
+});
 
 const workbook = XLSX.readFile("base.xlsx");
 const workbookSheets = workbook.SheetNames;
